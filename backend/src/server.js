9f36fe8622323438ba7413cb4bc04e2fc424eee5
Corno Kumar <corno25kumar@gmail.com>
2025-11-21 20:18:49 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,23 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDb } from "./lib/db.js";
+import cors from 'cors'
+import { functions, inngest } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+
+//middleware
+app.use(express.json())
+
+app.use(cors({
+  origin:ENV.CLIENT_URL,
+   credentials:true
+  }))
+
+  app.use("api/inngest", serve({client: inngest, functions}))
 
 app.get("/hat", (req, res) => {
   res.status(200).json("hello hat");
